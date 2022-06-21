f2bc3021b895284e09971cb6aa0b97e3248e99b1
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-06-21 20:21:31 +0530

    footer updated

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ const App = () => {
           </Typography.Title>
           <Space>
             <Link to="/">Home</Link>
-            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
             <Link to="/news">News</Link>
           </Space>
         </div>
