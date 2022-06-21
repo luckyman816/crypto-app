d90ae9715f667f95aa9b6169ebe30c310add7a32
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-06-21 19:14:25 +0530

    mobile responsive navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Menu, Typography, Avatar } from "antd";
 import { Link } from "react-router-dom";
 import {
@@ -12,6 +12,22 @@ import {
 import icon from "../images/cryptocurrency.png";
 
 const Navbar = () => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [screenSize, setScreenSize] = useState(null);
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (screenSize < 768) setActiveMenu(false);
+    else setActiveMenu(true);
+  }, [screenSize]);
+
   const MenuItems = [
     {
       key: "home",
@@ -42,9 +58,14 @@ const Navbar = () => {
         <Typography.Title level={2} className="logo">
           <Link to="/">Cryptoverse</Link>
         </Typography.Title>
-        {/* <Button className='menu-control-container'></Button> */}
+        <Button
+          className="menu-control-container"
+          onClick={() => setActiveMenu(!activeMenu)}
+        >
+          <MenuOutlined />
+        </Button>
       </div>
-      <Menu theme="dark" items={MenuItems}></Menu>
+      {activeMenu && <Menu theme="dark" items={MenuItems}></Menu>}
     </div>
   );
 };
