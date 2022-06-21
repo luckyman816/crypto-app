0cfd1d991c8d0054ea7e3d0525cba025c7d7a4b5
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-06-21 20:18:43 +0530

    exchanged completed

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Row, Typography } from "antd";
+
+import BgImage from "../images/coming-soon.png";
 
 const Exchanges = () => {
   return (
-    <div>
-      <h1>Exchanges</h1>
-    </div>
+    <Row gutter={[32, 32]} className="exchanges-card-container">
+      <img src={BgImage} alt="Comming Soon" className="exchanges-image" />
+      <Typography.Title level={1} className="exchanges-heading">
+        Comming Soon
+      </Typography.Title>
+    </Row>
   );
 };
 
