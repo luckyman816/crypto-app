0765cddec8c23aab5f0b943bb54973900c8943b9
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-06-21 09:28:24 +0530

    navbar component created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+
+import { Navbar } from "./components";
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main"></div>
+      <div className="footer"></div>
     </div>
   );
 };
