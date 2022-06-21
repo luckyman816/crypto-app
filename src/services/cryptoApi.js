bf1d5d3443cc9ee88ba5e75656de405ab0b759df
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-06-21 17:59:44 +0530

    crypto details added

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -16,7 +16,10 @@ export const cryptoApi = createApi({
     getCryptos: builder.query({
       query: (count) => createRequest(`/coins?limit=${count}`),
     }),
+    getCryptoDetails: builder.query({
+      query: (coinId) => createRequest(`/coin/${coinId}`),
+    }),
   }),
 });
 
-export const { useGetCryptosQuery } = cryptoApi;
+export const { useGetCryptosQuery, useGetCryptoDetailsQuery } = cryptoApi;
