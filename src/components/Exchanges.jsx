9eee6edd70c68f6bfead7532b7b3b90f0d321327
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2023-03-28 18:02:55 +0530

    Lazy loading added in coming soon image

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -7,7 +7,7 @@ import BgImage from "../images/coming-soon.png";
 const Exchanges = () => {
   return (
     <Row gutter={[32, 32]} className="exchanges-card-container">
-      <img src={BgImage} alt="Coming Soon" className="exchanges-image" />
+      <img src={BgImage} alt="Coming Soon" className="exchanges-image" loading="lazy" />
       <Typography.Title level={1} className="exchanges-heading">
         Coming Soon
       </Typography.Title>
